ce5f9c35ab4cb36f929cc2c2062ccc1c02348e77
Wemerson Martins <138730600+MartinsWemerson@users.noreply.github.com>
2024-03-21 21:30:09 -0300

    Add files via upload

---
+++ sccripts.js
@@ -0,0 +1,9 @@
+function enviar() {
+    let gmail = document.getElementById("gmail").value;
+    
+    if (gmail === "wemersongabriel2003@gmail.com") {
+        alert("Pode passar");
+    } else {
+        alert("Email incorreto");
+    }
+}
